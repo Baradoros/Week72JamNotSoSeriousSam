4f7f3f761115ab9ca53a6ec3d65feb1464816326
Baradoros <thatfunderburkguy@gmail.com>
2018-11-24 19:53:14 -0600

    cleaned up some code

--- Assets/Scripts/Test_Enemy_Miniguns.cs
+++ Assets/Scripts/Test_Enemy_Miniguns.cs
@@ -67,7 +67,6 @@ public class Test_Enemy_Miniguns : MonoBehaviour {
 
         // Check if HP <= 0 and kill this enemy if true
         if (health <= 0) {
-            Debug.Log("Health <= 0");
             Die();
         }
     }
