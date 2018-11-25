1ad280361278340e34a9d3939f4bb23c430019d8
Mary Gejda <ZuRyu@hotmail.co.uk>
2018-11-25 17:44:35 +0000

    Added main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public void StartGame()
+    {
+        GameManager.manager.LoadScene("Main", 0);
+    }
+
+    public void QuitGame()
+    {
+        GameManager.manager.Quit();
+    }
+}
